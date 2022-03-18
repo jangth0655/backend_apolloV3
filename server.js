0bfb76795b2535c395c798c3bcd30d057bc62964
JANGTAEHEE <jangth0655@gmail.com>
2022-03-19 01:07:02 +0900

    backend setup - install prisma

---
+++ server.js
@@ -0,0 +1,45 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
+`;
+
+// 모든 resolver들이 기본적으로 지니게 되는 것은
+// root, args, context, info
+
+const resolvers = {
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2022 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on  http://localhost:4000/"));
